6d94efc9ec6e80a68cbaa2c23ad57f81d01a5dd4
Victor Zhao <xinyunzhao@msn.com>
2016-04-10 17:43:37 -0400

    Merge with Xiangzhu's redesign of front page

---
+++ webserver/dashboard/static/dashboard/js/templatemo_script.js
@@ -0,0 +1,12 @@
+
+jQuery(function(){
+    $ = jQuery ;
+    //main menu
+    //banner slide
+    $('.banner').unslider({
+      // fluid: true,
+      autoplay: true,
+      infinite: true
+    });
+
+});
