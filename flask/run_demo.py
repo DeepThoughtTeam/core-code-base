fe1b8d5df80dd8ba4cca9bbbf532f3ef691cdd72
wei <weijiang@cmu.edu>
2016-04-20 13:01:23 -0400

    add activations in output file

--- flask/run_demo.py
+++ flask/run_demo.py
@@ -1,145 +1,75 @@
-'''
-	run_mlp:
-	when load a model, need to specify the shape of model
-'''
-
-import tensorflow as tf
 import numpy as np
-import input_data
-import sys, json
-import pickle
+import tensorflow as tf
+import os, pickle
 
-class INPUT_FLAG:
-	def __init__(self):
-		self.input_dim, self.trX, self.trY, self.teX, self.teY = \
-		None, None, None, None, None
+def one_hot(y, output_dim):
+	oh_y = np.zeros((len(y), output_dim))
+	for i in range(len(y)):
+		oh_y[i, int(y[i])] = 1
+	return oh_y
 
-def update_data_flag(
-	input_flag,
-	train_dir = "",
-	test_dir = "",
-	opt = "",
-	output_dim = 2,
-	mode = "train"):
-
-	if opt == "mnist" or opt == "MNIST":
-		mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-		input_flag.trX, input_flag.trY, input_flag.teX, input_flag.teY = \
-		mnist.train.images, mnist.train.labels, mnist.test.images, mnist.test.labels
-	else:
-		if mode == "train":
-			try:
-				train_data = np.loadtxt(open(train_dir,"rb"), delimiter=",", dtype=float)
-			except:
-				train_data = np.loadtxt(open(train_dir,"rb"), delimiter=" ", dtype=float)
-			input_flag.trX, input_flag.trY = train_data[:, :-1], train_data[:, -1]
-			input_flag.trY.astype(int)
-			temp_tr = np.zeros((len(input_flag.trY), output_dim))
-			for i in range(len(input_flag.trY)):
-				temp_tr[i, input_flag.trY[i]] = 1
-			input_flag.trY = temp_tr
-			input_flag.teX, input_flag.teY = input_flag.trX, input_flag.trY
-		elif mode == "test":
-			try:
-				test_data = np.loadtxt(open(test_dir,"rb"), delimiter=",", dtype=float)
-			except:
-				test_data = np.loadtxt(open(test_dir,"rb"), delimiter=" ", dtype=float)
-			input_flag.teX, input_flag.teY = test_data[:, :-1], test_data[:, -1]
-			input_flag.teY.astype(int)
-			temp_te = np.zeros((len(input_flag.teY), output_dim))
-			for i in range(len(input_flag.teY)):
-				temp_te[i, input_flag.teY[i]] = 1
-			input_flag.teY = temp_te
-	input_flag.input_dim = np.size(input_flag.teX, 1)
+def parse_input(input_file="data/param.txt"):
+	try:
+		train_data = np.loadtxt(open(input_file,"rb"), delimiter=",", dtype=float)
+	except:
+		train_data = np.loadtxt(open(input_file,"rb"), delimiter=" ", dtype=float)
+	return train_data[:, :-1], one_hot(train_data[:,-1], 2)
 
 def init_weights(shape):
-    return tf.Variable(tf.random_normal(shape, stddev=0.01))
+    return tf.Variable(tf.random_normal(shape, stddev=0.1))
 
-def model(X, input_dim, hidden_param, output_dim, activation_func = "relu"):
-	w_h, bias = [None] * len(hidden_param), [None] * (len(hidden_param) + 1)
-	w_h[0] = init_weights([input_dim, hidden_param[0]])
-	bias[0] = init_weights([hidden_param[0]])
-	for i in range(1, len(hidden_param)):
-		w_h[i] = init_weights([hidden_param[i-1], hidden_param[i]])
-		bias[i] = init_weights([hidden_param[i]])
-	
-        print hidden_param[-1], type(hidden_param[-1])
-        print output_dim, type(output_dim)
-	w_o = init_weights([hidden_param[-1], output_dim])
+def model(X, input_dim, hidden_weights, output_dim, act_func = tf.nn.relu):
+	w_h, bias = [None] * len(hidden_weights), [None] * (len(hidden_weights) + 1)
+	w_h[0] = init_weights([input_dim, hidden_weights[0]])
+	bias[0] = init_weights([hidden_weights[0]])
+	for i in range(1, len(hidden_weights)):
+		w_h[i] = init_weights([hidden_weights[i-1], hidden_weights[i]])
+		bias[i] = init_weights([hidden_weights[i]])
+	w_o = init_weights([hidden_weights[-1], output_dim])
 	bias[-1] = init_weights([output_dim])
 	
-	def activation(act_func, X, hidden_param, w_h, w_o, bias):
-		a = act_func(tf.matmul(X, w_h[0]) + bias[0])
-		for i in range(1, len(hidden_param)):
-			a = act_func(tf.matmul(a, w_h[i]) + bias[i])
-		return tf.matmul(a, w_o) + bias[-1]
+	acs = []
+	a = act_func(tf.matmul(X, w_h[0]) + bias[0])
+	acs.append(a)
+	for i in range(1, len(hidden_weights)):
+		a = act_func(tf.matmul(a, w_h[i]) + bias[i])
+		acs.append(a)
+	acs.append(act_func(tf.matmul(a, w_o) + bias[-1]))	
+	return (w_h, w_o, tf.matmul(a, w_o) + bias[-1], acs)
 
-	if activation_func == "relu":
-		return (w_h, w_o, activation(tf.nn.relu, X, hidden_param, w_h, w_o, bias))
-	elif activation_func == "sigmoid":
-		return (w_h, w_o, activation(tf.nn.sigmoid, X, hidden_param, w_h, w_o, bias))
-
-
-
-'''
-	warning: when run XOR experiment, num_iter should be large
-'''
-def run_mlp(
-	hidden_weights = [12], 
-	lr = 0.003, 
-	num_iter = 5, 
-	train_dir = "",
-	test_dir = "",
+def mlp(
+	train_x = None,
+	train_y = None,
+	test_x = None,
+	test_y = None,
+	hidden_weights = [3],
 	output_dim = 2,
+	lr = 0.01,
+	num_iter= 10,
 	saved_model_path = "model.ckpt",
 	saved_weights_path = "weights.json",
-	mode = "train",
-	output_file = "out_file",
-	opt = "user_data"):
+	out_file = 'output',
+	mode = "train"
+	):
 
-	input_flag = INPUT_FLAG()
-	if opt == "mnist":
-		output_dim = 11
-		update_data_flag(input_flag, "", "", opt, output_dim)
-	else:
-		update_data_flag(input_flag, train_dir, test_dir, output_dim, mode = mode)
-
-	trX, trY, teX, teY, input_dim = input_flag.trX, input_flag.trY, \
-	input_flag.teX, input_flag.teY, input_flag.input_dim
-
+	if mode=="train":
+		input_dim = train_x.shape[1]
+	elif mode=="test":
+		input_dim = test_x.shape[1]
 	X = tf.placeholder("float", [None, input_dim])
 	Y = tf.placeholder("float", [None, output_dim])
-        
-        print "I am in run_mlp"
-        print "hidden_weights is ", hidden_weights, "output_dim is ", output_dim
-	w_hs, w_o, activation = model(X, input_dim, hidden_weights, output_dim)
-
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(activation, Y))
-	# construct an optimizer, choice of learning rate
+	w_hs, w_o, activation, acs = model(X, input_dim, hidden_weights, output_dim)
+	cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(activation, Y))
 	optimizer = tf.train.RMSPropOptimizer(lr, 0.9).minimize(cost)
-
 	saver = tf.train.Saver()
 	sess = tf.Session()
 	sess.run(tf.initialize_all_variables())
-
-	out = open(output_file,'w')
-	if mode == "test":
-		saver.restore(sess, saved_model_path)
-		out.write(str(np.mean(np.argmax(teY, axis=1) == \
-			sess.run(tf.argmax(activation, 1), feed_dict={X: teX, Y: teY}))))
-	elif mode == "train":
+	out = open(out_file, 'w')
+	if mode == "train":
 		for i in range(num_iter):
-			if opt == "mnist" or opt == "MNIST":
-				for start, end in zip(range(0, len(trX), 50), range(50, len(trX), 50)):\
-				sess.run(optimizer, feed_dict={X: trX[start:end], Y: trY[start:end]})
-			else:
-				sess.run(optimizer, feed_dict={X: trX, Y: trY})
-			out.write(str(i) + ": "+ str(np.mean(np.argmax(trY, axis=1) == \
-				sess.run(tf.argmax(activation, 1), feed_dict={X: trX, Y: trY})))+"\n")
-			# print i,"'th loss: ",sess.run(cost, feed_dict={X: trX, Y: trY})
-		# save session
+			sess.run(optimizer, feed_dict={X:train_x , Y:train_y})
+			print i,":",sess.run(cost, feed_dict={X: train_x, Y: train_y})
 		saver.save(sess, saved_model_path)
-		# save weights as json
 		weights = sess.run(w_hs)
 		weights.append(sess.run(w_o))
 		for i in range(len(weights)):
@@ -148,53 +78,82 @@ def run_mlp(
 		with open(saved_weights_path, 'w') as f:
 			pickle.dump(weights, f)
 
-	else:
-		fsock = open('error.log', 'w')
-		sys.stderr = fsock
-		raise ValueError('Unidentified Option!')
+		# remove unnecessary files
+		os.remove(saved_model_path+".meta")
+		os.remove("checkpoint")
+
+		correct_prediction = tf.equal(tf.argmax(activation, 1), tf.argmax(Y, 1))
+		acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		out.write("training acc: " + str(sess.run(acc, feed_dict={X: train_x, Y: train_y})))
+	elif mode == "test":
+		print "start testing ... \n"
+		saver.restore(sess, "model.ckpt")
+		activations = sess.run(acs, feed_dict={X: test_x, Y: test_y})
+		out.write("testing accuracy: " + str(np.mean(np.argmax(test_y, axis=1) == \
+			np.argmax(activations[-1], 1))) + "\n")
+		activations[-1] = np.maximum(activations[-1], 0)
+		out.write("hidden layers setting: " + ' '.join(str(i) for i in hidden_weights) + '\n\n')
+		out.write("################## activations ################## \n")
+		out.write("* each row corresponds to one instance  \n\n")
+		for i in range(len(activations)):
+			if i != len(activations)-1:
+				out.write("hidden layer %d: \n" % i)
+			else:	
+				out.write("output layer: \n")
+			np.savetxt(out, activations[i], fmt='%0.3f')
+			out.write('\n')
 	out.close()
 
+
+
 def run_mlp_train(h_weights=[], lr = 0.001, it=500, train="", o_dim=2, model_path="", weights_path="", output=""):
-	run_mlp(lr=lr, hidden_weights=h_weights, num_iter=it, train_dir=train, output_dim=o_dim, mode="train", saved_model_path=model_path, saved_weights_path=weights_path, output_file=output)
-
-def run_mlp_test(h_weights=[], test="", model_path="", o_dim=2, output=""):
-	run_mlp(hidden_weights=h_weights, test_dir=test, saved_model_path=model_path ,mode="test", output_dim=2, output_file=output)
-	
-def main():
-	# # MNIST
-	# hidden_weights = [300, 65, 20]
-	# # train / test
-	# run_mlp(
-	# 	hidden_weights,
-	# 	num_iter = 5,
-	# 	mode = "test",
-	# 	opt = "mnist",
-	# 	saved_model_path = "model.ckpt",
-	# output_file = "output.txt")
-
-	# XOR
-
-	# train
-	run_mlp(
-		hidden_weights = [3,5], 
-		num_iter = 10, 
-		train_dir = "sample_train.txt", 
-		output_dim = 2,
-		mode = "train",
-		saved_model_path = "model.ckpt",
-		saved_weights_path = "weights.json",
-		output_file = "output.txt"
+	x, y = parse_input(train)
+	mlp(train_x = x,
+		train_y = y,
+		lr=lr, 
+		hidden_weights=h_weights, 
+		num_iter=it, 
+		output_dim=o_dim, 
+		saved_model_path=model_path, 
+		saved_weights_path=weights_path, 
+		out_file=output,
+		mode="train"
 		)
 
-	# # test
-	# run_mlp(
-	# 	hidden_weights = [6],
-	# 	test_dir = "sample_train.txt",
-	# 	saved_model_path = "model.ckpt",
-	# 	output_dim = 2,
-	# 	mode = "test",
-	# 	output_file = "output.txt"
+def run_mlp_test(h_weights=[], test="", model_path="", o_dim=2, output=""):
+	x, y = parse_input(test)
+	mlp(
+		test_x = x,
+		test_y = y,
+		hidden_weights=h_weights, 
+		output_dim=o_dim, 
+		saved_model_path=model_path, 
+		out_file=output,
+		mode="test"
+		)
+
+def main():
+	data_dir = "data/diamod_train.txt"
+ 
+	# # train
+	# run_mlp_train(
+	# h_weights=[5,5], 
+	# 	lr=0.01, 
+	# 	it=1000, 
+	# 	train=data_dir, 
+	# 	o_dim=2, 
+	# 	model_path="model.ckpt", 
+	# 	weights_path="weights.json", 
+	# 	output="output"
 	# 	)
+	
+	# test
+	run_mlp_test(
+		h_weights=[5,5], 
+		test=data_dir, 
+		model_path="model.ckpt", 
+		o_dim=2, 
+		output="output")
 
 if __name__ == "__main__":
 	main()
